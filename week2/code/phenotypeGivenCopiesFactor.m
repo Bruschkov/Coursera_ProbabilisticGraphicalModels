f0bb886216d58ed44fe63d059e8409657cfe3d0b
jan.brusch <jan.brusch@neuland-bfi.de>
2019-02-02 20:10:30 +0100

    decoupledFactor

--- week2/code/phenotypeGivenCopiesFactor.m
+++ week2/code/phenotypeGivenCopiesFactor.m
@@ -59,9 +59,20 @@ phenotypeFactor = struct('var', [], 'card', [], 'val', []);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 
 % Fill in phenotypeFactor.var.  This should be a 1-D row vector.
+phenotypeFactor.var = [phenotypeVar, geneCopyVarOne, geneCopyVarTwo]
 % Fill in phenotypeFactor.card.  This should be a 1-D row vector.
+phenotypeFactor.card = [2, numAlleles, numAlleles]
 
 phenotypeFactor.val = zeros(1, prod(phenotypeFactor.card));
 % Replace the zeros in phentoypeFactor.val with the correct values.
-
+idx = 1;
+for i=1:numAlleles
+    for j=1:numAlleles
+        g = allelesToGenotypes(j, i);
+        p = alphaList(g);
+        phenotypeFactor.val(idx) = p;
+        phenotypeFactor.val(idx+1) = 1-p;
+        idx += 2;
+    end
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
