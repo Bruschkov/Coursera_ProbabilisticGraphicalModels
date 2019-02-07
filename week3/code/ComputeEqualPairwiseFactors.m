cd0ec279955d3906902e1aa670be13212fdaabd2
jan.brusch <jan.brusch@neuland-bfi.de>
2019-02-07 22:40:36 +0100

    part2 done

--- week3/code/ComputeEqualPairwiseFactors.m
+++ week3/code/ComputeEqualPairwiseFactors.m
@@ -19,5 +19,10 @@ n = length(images);
 factors = repmat(struct('var', [], 'card', [], 'val', []), n - 1, 1);
 
 % Your code here:
+for i=1:length(factors)
+    factors(i).var = [i, i+1];
+    factors(i).card = [K, K];
+    factors(i).val = ones(1,K*K);
+end
 
 end
